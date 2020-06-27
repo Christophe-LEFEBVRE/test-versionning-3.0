ab91ef4a62bd90f69e0e95bc761f6075367774d7
Christophe LEFEBVRE <cloeuf@gmail.com>
2020-06-27 15:23:29 +0200

    Initial Commit

---
+++ test versionning/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test versionning
+//
+//  Created by Christophe LEFEBVRE on 27/06/2020.
+//  Copyright © 2020 Christophe LEFEBVRE. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ test versionning/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test versionning
+//
+//  Created by Christophe LEFEBVRE on 27/06/2020.
+//  Copyright © 2020 Christophe LEFEBVRE. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
